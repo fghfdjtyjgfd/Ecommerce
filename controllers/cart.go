9e3735bcf818ae1766466235e541c646f70e0934
fghfdjtyjgfd <fghfdjtyjgfd@hotmail.com>
2024-04-27 12:46:44 +0700

    added func getItemFromCart

--- controllers/cart.go
+++ controllers/cart.go
@@ -8,10 +8,12 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"ecommerce/database"
+	"ecommerce/models"
 )
 
 type Application struct {
@@ -92,7 +94,48 @@ func (app *Application) RemoveItem() gin.HandlerFunc {
 }
 
 func GetItemFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, "invalid id")
+			c.Abort()
+			return
+		}
 
+		usert_id, _ := primitive.ObjectIDFromHex(user_id)
+		var ctx, cancel = context.WithTimeout(context.Background(), time.Second*100)
+		defer cancel()
+
+		var filledcart models.User
+		err := UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: usert_id}}).Decode(&filledcart)
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(500, "Not found")
+			return
+		}
+
+		filter_match := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: usert_id}}}}
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$usercart"}}}}
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value: "$_id"}, {Key: "total", Value: bson.D{primitive.E{Key: "$sum", Value: "$usercart.price"}}}}}}
+		pointcursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filter_match, unwind, grouping})
+		if err != nil {
+			log.Println(err)
+		}
+		var listing []bson.M
+		if err = pointcursor.All(ctx, &listing); err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+		for _, json := range listing{
+			c.IndentedJSON(200, json["total"])
+			c.IndentedJSON(200, filledcart.UserCart)
+		}
+		ctx.Done()
+
+
+
+	}
 }
 
 func (app *Application) BuyFromCart() gin.HandlerFunc {
